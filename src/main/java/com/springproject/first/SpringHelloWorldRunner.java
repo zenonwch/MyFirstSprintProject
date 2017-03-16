4cf5a25810f83dde2e18d362f996ecdc29248d6b
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-03-16 21:50:20 +0200

    Spring ApplicationContext Container implementation using FileSystemXmlApplicationContext

--- src/main/java/com/springproject/first/SpringHelloWorldRunner.java
+++ src/main/java/com/springproject/first/SpringHelloWorldRunner.java
@@ -3,17 +3,26 @@ package main.java.com.springproject.first;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 public class SpringHelloWorldRunner {
-    //Using ApplicationContext Container
+    //----- Using ApplicationContext Container -----//
+    // ClassPathXmlApplicationContext
     public static void main(final String[] args) {
         final ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
         final SpringHelloWorld helloWorld = (SpringHelloWorld) ctx.getBean("springHelloWorld");
         helloWorld.getMessage();
     }
 
-    //Using Spring BeanFactory Container
+    // FileSystemXmlApplicationContext
+//    public static void main(final String[] args) {
+//        final ApplicationContext ctx = new FileSystemXmlApplicationContext("src/resources/applicationContext.xml");
+//        final SpringHelloWorld message = (SpringHelloWorld) ctx.getBean("springHelloWorld");
+//        message.getMessage();
+//    }
+
+    //----- Using Spring BeanFactory Container -----//
 //    public static void main(final String[] args) {
 //        final XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("applicationContext.xml"));
 //        final SpringHelloWorld message = (SpringHelloWorld) factory.getBean("springHelloWorld");
